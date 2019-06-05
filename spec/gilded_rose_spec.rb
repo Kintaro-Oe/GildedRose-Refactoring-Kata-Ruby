074fdc5648f65446c97e08f6fa37ea286094b870
Kintaro-Oe <marcusclairmont@gmail.com>
2019-06-05 18:16:48 +0100

    #update_quality does not remove the quality value

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -13,5 +13,10 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].sell_in).to be_an_instance_of(Integer)
     end
+    it "does not remove the quality value" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to be_an_instance_of(Integer)
+    end
   end
 end
