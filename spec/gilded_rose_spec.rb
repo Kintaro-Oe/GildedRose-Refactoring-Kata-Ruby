687db9cdec8f7d8eebe781656deebd249cca2cb5
Kintaro-Oe <marcusclairmont@gmail.com>
2019-06-05 18:40:02 +0100

    #update_quality does not alters the sell_in or quality of "Sulfuras, Hand of Ragnaros"

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -44,5 +44,11 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq(50)
     end
+    it "does not alters the sell_in or quality of 'Sulfuras, Hand of Ragnaros'" do
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 50)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq(0)
+      expect(items[0].quality).to eq(50)
+    end
   end
 end
