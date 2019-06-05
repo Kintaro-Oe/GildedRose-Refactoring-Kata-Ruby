32af0cc51e14adee79c4ad25a221391d820475c8
Kintaro-Oe <marcusclairmont@gmail.com>
2019-06-05 18:21:08 +0100

    #update_quality lowers both quality and sell_in values for every item

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -18,5 +18,11 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to be_an_instance_of(Integer)
     end
+    it "lowers both quality and sell_in values for every item" do
+      items = [Item.new("foo", 5, 5)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq(4)
+      expect(items[0].quality).to eq(4)
+    end
   end
 end
