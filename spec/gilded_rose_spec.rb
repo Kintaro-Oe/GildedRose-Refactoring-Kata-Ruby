051330314b15e10ce0f41a45c14211a5ea6d4598
Kintaro-Oe <marcusclairmont@gmail.com>
2019-06-05 18:35:40 +0100

    #update_quality increases the quality of "Aged Brie" by 2 as it gets older

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -34,5 +34,10 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq(0)
     end
+    it "increases the quality of 'Aged Brie' by 2 as it gets older" do
+      items = [Item.new("Aged Brie", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(2)
+    end
   end
 end
