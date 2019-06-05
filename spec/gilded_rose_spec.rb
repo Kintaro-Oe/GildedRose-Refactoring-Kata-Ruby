885802dcf1ad1c391a171c27c65e826693208443
Kintaro-Oe <marcusclairmont@gmail.com>
2019-06-05 18:14:57 +0100

    #update_quality does not remove the sell_in value

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -8,6 +8,10 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+    it "does not remove the sell_in value" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to be_an_instance_of(Integer)
+    end
   end
-
 end
