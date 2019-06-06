3ece7b46a666610f9c39f8ed9d171b278d1c07a6
Kintaro-Oe <marcusclairmont@gmail.com>
2019-06-06 17:47:01 +0100

    refactor negative #not methods to be positive, place positive cases first in conditionals with else

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -8,9 +8,7 @@ class GildedRose
   def update_quality()
     @items.each do |item|
 
-      if not_aged_brie?(item) && not_backstage_pass?(item)
-        decrease_quality(item)
-      else
+      if aged_brie?(item) || backstage_pass?(item)
         increase_quality(item)
         if item.name == "Backstage passes to a TAFKAL80ETC concert"
           if item.sell_in < 11
@@ -20,19 +18,21 @@ class GildedRose
             increase_quality(item)
           end
         end
+      else
+        decrease_quality(item)
       end
 
       decrease_sell_in(item)
 
       if item.sell_in < 0
-        if not_aged_brie?(item)
-          if not_backstage_pass?(item)
-            decrease_quality(item)
-          else
-            item.quality = 0
-          end
-        else
+        if aged_brie?(item)
           increase_quality(item)
+        else
+          if backstage_pass?(item)
+            item.quality = 0
+          else
+            decrease_quality(item)
+          end
         end
       end
     end
@@ -43,27 +43,29 @@ class GildedRose
   end
 
   def decrease_quality(item)
-    item.quality -= 1 if not_sulfuras?(item) && item.quality > 0
+    if item.quality > 0
+      item.quality -= 1 unless sulfuras?(item)
+    end
   end
 
   def decrease_sell_in(item)
-    item.sell_in -= 1 if not_sulfuras?(item)
+    item.sell_in -= 1 unless sulfuras?(item)
   end
 
   def below_max_quality?(item)
     item.quality < MAX_QUALITY
   end
 
-  def not_sulfuras?(item)
-    item.name != "Sulfuras, Hand of Ragnaros"
+  def sulfuras?(item)
+    item.name == "Sulfuras, Hand of Ragnaros"
   end
 
-  def not_aged_brie?(item)
-    item.name != "Aged Brie"
+  def aged_brie?(item)
+    item.name == "Aged Brie"
   end
 
-  def not_backstage_pass?(item)
-    item.name != "Backstage passes to a TAFKAL80ETC concert"
+  def backstage_pass?(item)
+    item.name == "Backstage passes to a TAFKAL80ETC concert"
   end
 end
 
