ec14596306181ad8dc2fa287d8e9d8feae70ac98
Kintaro-Oe <marcusclairmont@gmail.com>
2019-06-06 12:36:13 +0100

    improve test descriptions

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -8,12 +8,12 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
-    it "does not remove the sell_in value" do
+    it "does not change sell_in value from an Integer" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].sell_in).to be_an_instance_of(Integer)
     end
-    it "does not remove the quality value" do
+    it "does not change quality value from an Integer" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to be_an_instance_of(Integer)
