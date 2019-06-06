cf025e8985dff43f9b853ce68011eb8dec1ac62a
Kintaro-Oe <marcusclairmont@gmail.com>
2019-06-06 13:16:12 +0100

    remove repetition from value setting

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -10,25 +10,25 @@ class GildedRose
       if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+            item.quality -= 1
           end
         end
       else
         if below_max_quality?(item)
-          item.quality = item.quality + 1
+          item.quality += 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
-                item.quality = item.quality + 1 if below_max_quality?(item)
+                item.quality += 1 if below_max_quality?(item)
             end
             if item.sell_in < 6
-                item.quality = item.quality + 1 if below_max_quality?(item)
+                item.quality += 1 if below_max_quality?(item)
             end
           end
         end
       end
 
       if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
+        item.sell_in -= 1
       end
 
       if item.sell_in < 0
@@ -36,14 +36,14 @@ class GildedRose
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
               if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
+                item.quality -= 1
               end
             end
           else
-            item.quality = item.quality - item.quality
+            item.quality = 0
           end
         else
-            item.quality = item.quality + 1 if below_max_quality?(item)
+            item.quality += 1 if below_max_quality?(item)
         end
       end
     end
